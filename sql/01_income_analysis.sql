a44767c8bad1871d6abef3d05d02c77de74492fe
itssandhiya2005-cmyk <itssandhiya2005@gmail.com>
2026-07-11 12:45:03 +0530

    ADD income analysis SQL queries

--- sql/01_income_analysis.sql
+++ sql/01_income_analysis.sql
@@ -1,1 +1,32 @@
-sql/01_income_analysis.sql
+CREATE DATABASE reliance_analysis;
+USE reliance_analysis;
+SELECT * FROM reliance_income_cleaned;
+
+SELECT `Year`, `Total Revenue`
+FROM reliance_income_cleaned
+ORDER BY `Year`;
+
+SELECT Year, `Total Revenue`
+FROM reliance_income_cleaned
+ORDER BY CAST(`Total Revenue` AS DECIMAL(30,2)) DESC
+LIMIT 1;
+
+SELECT `Year`, `Total Revenue`, `Net Income Common Stockholders`,
+ROUND(
+(CAST(`Net Income Common Stockholders` AS DECIMAL(30,2)) / CAST(`Total Revenue` AS DECIMAL(30,2))) * 100, 2) AS Profit_Margin_Percent
+FROM reliance_income_cleaned
+ORDER BY `Year` DESC;
+
+SELECT Year,
+`Operating Income`,`Net Income Common Stockholders`,
+CAST(`Operating Income` AS DECIMAL (30,2)) - CAST(`Net Income Common Stockholders` AS DECIMAL (30,2)) AS  Deductions
+ FROM reliance_income_cleaned
+ ORDER BY YEAR
+ 
+SELECT Year, `Tax Provision`, `Total Revenue`,
+       ROUND(
+           (CAST(`Tax Provision` AS DECIMAL(30,2)) /
+           CAST(`Total Revenue` AS DECIMAL(30,2))) * 100
+       , 2) AS Tax_Percent
+FROM reliance_income_cleaned
+ORDER BY Year;
