590ac01d0f3165dcae7ad40d3a45964438e1183e
itssandhiya2005-cmyk <itssandhiya2005@gmail.com>
2026-07-11 12:46:39 +0530

    Add cash flow analysis queries to SQL file

--- sql/03_cashflow_analysis.sql
+++ sql/03_cashflow_analysis.sql
@@ -1,1 +1,14 @@
+SELECT * FROM reliance_cashflow_cleaned
+ORDER BY Year;
 
+SELECT year,
+ROUND(CAST(`Operating Cash Flow` AS DECIMAL (30,2)) / 10000000, 2) AS Operating_Cashflow_crores,
+ROUND(CAST(`Investing Cash Flow`AS DECIMAL (30,2)) / 10000000, 2) AS Investing_Cashflow_crores,
+ROUND(CAST(`Financing Cash Flow`AS DECIMAL (30,2)) / 10000000, 2) AS Financing_Cashflow_crores
+FROM reliance_cashflow_cleaned
+ORDER BY Year;
+
+SELECT year,
+ROUND(CAST(`Free Cash Flow` AS DECIMAL (30,2)) / 10000000, 2) AS Free_Cash_Flow_Crores
+FROM reliance_cashflow_cleaned
+ORDER BY Year;
